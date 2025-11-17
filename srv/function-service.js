9e1338f738c721104b64cb100ad5021f9f8aa25f
Christian Ott <ott@illucit.com>
2025-11-17 10:53:31 +0100

    Adjust app to reproduce the error with ODataV2 function returns

--- srv/function-service.js
+++ srv/function-service.js
@@ -1,26 +1,7 @@
 const cds = require('@sap/cds')
 
-const log = cds.log('custom-cors', 'info')
-
-cds.on('bootstrap', async app => {
-	app.use((req, res, next) => {
-
-		req.res.setHeader("access-control-allow-origin", "http://localhost:4004");
-		res.setHeader("access-control-allow-origin", "http://localhost:4004");
-		next()
-	})
-})
-
 module.exports = class AdminService extends cds.ApplicationService {
     init() {
-		this.on('bootstrap', async app => {
-			app.use((req, res, next) => {
-				log("Bootstrapping Request")
-				req.res.setHeader("access-control-allow-origin", "http://localhost:4004");
-				res.setHeader("access-control-allow-origin", "http://localhost:4004");
-				next()
-			})
-		})
 
         this.on("ping", async req  => {
             return "Pong!";
